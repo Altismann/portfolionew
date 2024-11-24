b58e6a09639cf4e713be6df1a39c90d2215b8600
Altismann <87611738+Altismann@users.noreply.github.com>
2024-11-24 17:59:34 +0000

    sdfssd

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const { i18n } = require('./next-i18next.config')
-
-module.exports = {
-  i18n,
-}
-
 
 export default nextConfig;
