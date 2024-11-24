8f2836bfd15ba40a6999363e0529156ce0b14d0e
Altismann <87611738+Altismann@users.noreply.github.com>
2024-11-24 17:40:24 +0000

    gf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,23 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Sitecraft Portfolio",
+  description: "Hej! Jag heter Anders... en produktdesigner och webbutvecklare plaserad i Stockholm. Jag skapar enklare gränssnitt för hobbyister och skapare.",
+};
 
 
-
-
-
-export const metadata = {
-  metadataBase: new URL('https://acme.com'),
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en-US',
-      'de-DE': '/de-DE',
-    },
-  },
-  openGraph: {
-    images: '/og-image.png',
-  },
 }
 
 
